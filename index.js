36196292f37839ba6a35d93d0346b841397a8cc2
Pablo Tachella <pablotachella95@gmail.com>
2020-05-29 14:57:03 -0300

    Se agrego iluminacion de las secuencias y la obtencion del imput del usuario

--- index.js
+++ index.js
@@ -7,10 +7,90 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego{
     constructor(){
         this.inicializar()
+        this.generarSecuencia()
+        this.siguienteNivel()
     }
 
     inicializar(){
+        this.elegirColor = this.elegirColor.bind(this)
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia(){
+        this.secuencia = new Array(10)
+            // map() necesita que los elementos del array esten inicializados, con fill(0) estoy
+            // diciendo que se inicialicen en 0
+            .fill(0)
+            .map(n => Math.floor(Math.random () * 4))
+    }
+
+    siguienteNivel(){
+        this.iluminarSecuencia()
+        this.agregarEventosClick()
+    }
+
+    transformarNumeroAColor(num){
+        switch(num){
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2:
+                return 'naranja'
+            case 3:
+                return 'verde' 
+        }
+    }
+
+    transformarColorANumero(color){
+        switch(color){
+            case 'celeste':
+                return 0
+            case 'violeta':
+                return 1
+            case 'naranja':
+                return 2
+            case 'verde':
+                return 3 
+        }
+    }
+
+    iluminarSecuencia(){
+        for(var i = 0; i < this.nivel; i++){
+            const num = this.secuencia[i]
+            const color = this.transformarNumeroAColor(num)
+            setTimeout(() => this.iluminarColor(color), i * 1000)
+        }
+    }
+
+    iluminarColor(color){
+        this.colores[color].classList.add('light')
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+
+    apagarColor(color){
+        this.colores[color].classList.remove('light')
+    }
+
+    agregarEventosClick(){
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+    }
+
+    elegirColor(ev){
+        const colorElegido = ev.target.dataset.color
+        const numeroColor = this.transformarColorANumero(colorElegido)
+
+        this.iluminarColor(colorElegido)
     }
 }
 
@@ -18,6 +98,6 @@ class Juego{
 
 function empezarJuego(){
 
-    const juego = new Juego()
+    window.juego = new Juego()
 }
 
