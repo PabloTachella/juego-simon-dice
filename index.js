56b9568b3bbac197910c40881a23f2d05247030f
Pablo Tachella <pablotachella95@gmail.com>
2020-05-29 20:00:47 -0300

    Se completo el script para el ciclo del juego, se agregaron alert con libreria externa y se agrego un contador

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
+const contador = document.getElementById('contador')
+const ULTIMO_NIVEL = 10
 
 class Juego{
     constructor(){
+        this.inicializar = this.inicializar.bind(this)
         this.inicializar()
         this.generarSecuencia()
-        this.siguienteNivel()
+        setTimeout(this.siguienteNivel, 500)
     }
 
     inicializar(){
         this.elegirColor = this.elegirColor.bind(this)
-        btnEmpezar.classList.add('hide')
+        this.siguienteNivel = this.siguienteNivel.bind(this)
+        this.iniciarContador()
+        this.toggleBtnEmpezar()
         this.nivel = 1
         this.colores = {
             celeste,
@@ -23,8 +28,16 @@ class Juego{
         }
     }
 
+    toggleBtnEmpezar(){
+        if (btnEmpezar.classList.contains('hide')){
+            btnEmpezar.classList.remove('hide')
+        } else {
+            btnEmpezar.classList.add('hide')
+        }
+    }
+
     generarSecuencia(){
-        this.secuencia = new Array(10)
+        this.secuencia = new Array(ULTIMO_NIVEL)
             // map() necesita que los elementos del array esten inicializados, con fill(0) estoy
             // diciendo que se inicialicen en 0
             .fill(0)
@@ -32,8 +45,10 @@ class Juego{
     }
 
     siguienteNivel(){
+        this.subNivel = 0
         this.iluminarSecuencia()
         this.agregarEventosClick()
+        this.actualizarContador()
     }
 
     transformarNumeroAColor(num){
@@ -86,11 +101,55 @@ class Juego{
         this.colores.verde.addEventListener('click', this.elegirColor)
     }
 
+    eliminarEventosClick(){
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)
+    }
+
+    iniciarContador(){
+        contador.value = `Nivel: 0`
+    }
+
+    actualizarContador(){
+        contador.value = `Nivel: ${this.nivel}`
+    }
+
     elegirColor(ev){
         const colorElegido = ev.target.dataset.color
         const numeroColor = this.transformarColorANumero(colorElegido)
 
         this.iluminarColor(colorElegido)
+        if (numeroColor === this.secuencia[this.subNivel]){
+            this.subNivel++
+            if(this.subNivel === this.nivel){
+                this.nivel++
+                this.eliminarEventosClick()
+                if (this.nivel === (ULTIMO_NIVEL +1)){
+                    this.ganoElJuego()
+                } else {
+                    setTimeout(this.siguienteNivel, 1000)
+                }
+            }
+        } else {
+            this.perdioElJuego()
+        }
+    }
+
+    ganoElJuego(){
+        this.iniciarContador()
+        swal('Felicitaciones!', 'Ganaste, demostraste no apestar tanto', 'success')
+            .then(this.inicializar)
+    }
+
+    perdioElJuego(){
+        this.iniciarContador()
+        swal('JAA JA JA JAAA!', 'Perdiste, seguí participando', 'error')
+            .then(() => {
+                this.eliminarEventosClick()
+                this.inicializar()
+            })
     }
 }
 
